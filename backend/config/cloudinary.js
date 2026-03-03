f50af3ff80ff06119bcb4c45097c6bb350e23b36
Rahul Metre <34718002+RahulMetre03@users.noreply.github.com>
2026-03-03 23:59:25 +0530

    Update cloudinary.js

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -1,9 +1,9 @@
 import { v2 as cloudinary } from "cloudinary";
 
 cloudinary.config({
-  cloud_name: "deutxiah4",
-  api_key: "618576369687239",
-  api_secret: "UC-6nAcPNvFqpZJAuu7XgwgPr-s",
+  cloud_name: "",
+  api_key: "",
+  api_secret: "",
   secure: true
 });
 
