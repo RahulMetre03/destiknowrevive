cb1a3da96784b21da007575f41090a8efdaf9015
Rahul Metre <34718002+RahulMetre03@users.noreply.github.com>
2026-04-14 22:55:04 +0530

    Update socialApiService.js

--- frotnend/src/services/socialApiService.js
+++ frotnend/src/services/socialApiService.js
@@ -1,5 +1,5 @@
 // services/socialApiService.js
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://destiknowrevive.onrender.com/api';
 
 const getAuthHeaders = () => ({
     'Authorization': `Bearer ${localStorage.getItem('token') || ''}`,
